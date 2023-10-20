90c1875f90c0b81c01df1857871d9bf869a8b1ad
zxdawn <xinzhang1215@gmail.com>
2023-10-20 14:05:16 +0200

    upload environment files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+requires = ['matplotlib >=3.8.0', 'numpy', 'pandas', 'scikit-learn', 'scikit-image', 'scipy',
+            'xarray', 'cfgrib', 'h5netcdf', 'rioxarray', 'pyresample', 'folium', 'contextily',
+            'enpt', 'satpy', 'algotom', 'dem_stitcher', 'aiohttp']
+
+setup(
+    name='hyperch4',
+    description= 'Python package for hyperspectral satellite imaging of methane',
+    version= '0.1.0',
+    author = 'Xin Zhang',
+    packages=find_packages(),
+    install_requires=requires,
+    python_requires='>=3.10'
+    )
