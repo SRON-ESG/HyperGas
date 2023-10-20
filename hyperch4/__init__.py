e55e46df299b46608b9ab7246047f1df0592bc39
zxdawn <xinzhang1215@gmail.com>
2023-10-20 14:37:34 +0200

    upload init file

---
+++ hyperch4/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 HyperCH4 developers
+#
+# This file is part of hyperch4.
+#
+# hyperch4 is a library to retrieve methane from hyperspectral satellite data
+"""HyperCH4 package initializer"""
+
+from hyperch4.hyper import Hyper
