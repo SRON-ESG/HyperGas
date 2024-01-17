4f99cc9af744a51353a999af796487dae7b10450
zxdawn <xinzhang1215@gmail.com>
2024-01-17 16:12:05 +0100

    update requirement

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@ from setuptools import setup, find_packages
 
 requires = ['matplotlib >=3.8.0', 'numpy', 'pandas', 'scikit-learn', 'scikit-image', 'scipy',
             'xarray', 'cfgrib', 'h5netcdf', 'rioxarray', 'pyresample', 'folium', 'contextily',
-            'enpt', 'satpy', 'algotom', 'dem_stitcher', 'aiohttp', 'spacetrack'
+            'enpt', 'satpy', 'algotom', 'dem_stitcher', 'aiohttp', 'spacetrack', 'roaring-landmask'
             ]
+extras_require = {
+    'doc': ['sphinx', 'sphinx_rtd_theme'],
+}
+all_extras = []
+for extra_deps in extras_require.values():
+    all_extras.extend(extra_deps)
+extras_require["all"] = list(set(all_extras))
 
 setup(
     name='hypergas',
@@ -12,5 +19,6 @@ setup(
     author = 'Xin Zhang',
     packages=find_packages(),
     install_requires=requires,
-    python_requires='>=3.10'
+    extras_require=extras_require,
+    python_requires='>=3.10',
     )
