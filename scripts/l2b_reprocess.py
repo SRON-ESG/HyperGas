debd4c4b0f6edbb194ac945545374afa815623ec
zxdawn <xinzhang1215@gmail.com>
2024-02-06 13:37:47 +0100

    upload L2 reprocessing script

---
+++ scripts/l2b_reprocess.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023-2024 HyperGas developers
+#
+# This file is part of hypergas.
+#
+# hypergas is a library to retrieve trace gases from hyperspectral satellite data
+"""Regenerate L2B NetCDF products with L3 plume masks."""
+
+import logging
+import os
+from glob import glob
+from itertools import chain
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+from pyresample import geometry, kd_tree
+
+from l2b_process import L2B
+from utils import get_dirs
+
+# set the logger level
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s: %(message)s',
+                    datefmt='%Y/%m/%d %H:%M:%S')
+LOG = logging.getLogger(__name__)
+
+# set filename pattern to load data automatically
+PATTERNS = ['ENMAP01-____L3B*plume0.nc', 'EMIT_L3B*plume0.nc', 'PRS_L3_*plume0.nc']
+
+
+def reprocess_data(filename, prefix, species, land_mask_source, rad_dist):
+    """Reprocess L2 by L3 plume masks"""
+    # read all L3 nc data with same prefix
+    l3_filelist = glob(prefix+'*nc')
+    ds = xr.open_mfdataset(l3_filelist,
+                           concat_dim=[pd.Index(np.arange(len(l3_filelist)), name='plume_id')],
+                           combine='nested',
+                           )
+
+    # create mask, 0 (plume): not included in matched filter
+    mask = (~ds['ch4'].isnull()).sum(dim='plume_id')
+    mask = mask == 0
+    swath_def = geometry.SwathDefinition(lons=ds['longitude'], lats=ds['latitude'])
+
+    # read L1 data
+    l2b_scene = L2B(ds.attrs['filename'], species=species, skip_exist=False)
+    rads_area = l2b_scene.hyp.scene['radiance'].attrs['area']
+
+    mask_resample = kd_tree.resample_nearest(swath_def, mask.values, rads_area, radius_of_influence=100)
+    mask_resample = xr.DataArray(mask_resample, dims=['y', 'x']).rename('plume_mask')
+
+    # run retrieval and save L2 NetCDF file
+    l2b_scene.retrieve(plume_mask=mask_resample, land_mask_source=land_mask_source, rad_dist=rad_dist)
+    l2b_scene.denoise()
+    l2b_scene.ortho()
+    l2b_scene.to_netcdf()
+
+
+def main():
+    species = 'ch4'
+    rad_dist = 'normal'
+    land_mask_source = 'GSHHS'  # 'GSHHS' or 'Natural Earth'
+
+    # get the filname list
+    filelist = list(chain(*[glob(os.path.join(data_dir, pattern), recursive=True) for pattern in PATTERNS]))
+    filelist = list(sorted(filelist))
+
+    if len(filelist) == 0:
+        return
+
+    for file_plume0 in filelist:
+        # get file with same prefix because one scene may have multiple plumes
+        prefix = file_plume0.split('plume0')[0]
+        filenames = glob(f"{prefix}*.nc")
+        for filename in filenames:
+            LOG.info(f"Reprocessing {prefix.replace('L3', 'L2')} ...")
+            reprocess_data(filename, prefix, species, land_mask_source, rad_dist)
+
+
+if __name__ == '__main__':
+    # root dir of hyper data
+    root_dir = '/data/xinz/Hyper_TROPOMI_plume/'
+    lowest_dirs = get_dirs(root_dir)
+
+    if input("This script will update all L2 NetCDF files with L3 plume masks. Are you sure? (y/n)") != "y":
+        exit()
+
+    for data_dir in lowest_dirs:
+        LOG.info(f'Reprocessing data under {data_dir}')
+        main()
