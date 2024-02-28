b3525982f32784df7d4add01fc414f97ede842db
zxdawn <xinzhang1215@gmail.com>
2024-02-28 12:05:20 +0100

    update vmax to 99th percentile for denoised data

--- hypergas/folium_map.py
+++ hypergas/folium_map.py
@@ -164,9 +164,9 @@ class Map():
                 if 'segmentation' in self.ds.keys():
                     # if the data is masked, we choose the data with label >=1
                     #   which means "land" type or "not background" classification
-                    cmap_vmax = da_ortho.where(self.ds['segmentation']>=1).max()
+                    cmap_vmax = da_ortho.where(self.ds['segmentation']>=1).quantile(0.99)
                 else:
-                    cmap_vmax = da_ortho.max()
+                    cmap_vmax = da_ortho.quantile(0.99)
 
                 # crop cmap_max to max(vmax, vmin+1)
                 if vmax is not None:
