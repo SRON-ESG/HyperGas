9546e565fbb419df3b5a9b84440ddec072de6ee9
zxdawn <xinzhang1215@gmail.com>
2024-06-26 15:55:20 +0200

    reprocess nc by default

--- scripts/l3b_reprocess.py
+++ scripts/l3b_reprocess.py
@@ -40,13 +40,16 @@ INSTITUTION = 'SRON Netherlands Institute for Space Research'
 PATTERNS = ['ENMAP01-____L3B*.csv', 'EMIT_L3B*.csv', 'PRS_L3_*.csv']
 
 
-def reprocess_data(filename, reprocess_nc, wind_data):
+def reprocess_data(filename, wind_data):
     LOG.info('Reading csv and nc files ...')
     df = pd.read_csv(filename)
     plume_filename = filename.replace('.csv', '.nc')
+
+    # read settings in csv file
     azimuth_diff_max = df['azimuth_diff_max'].item()
     ipcc_sector = df['ipcc_sector'].item()
     gas = df['gas'].item().lower()
+    name = df['name'].item()
     land_only = df['land_only'].item()
     land_mask_source = df['land_mask_source'].item()
 
@@ -59,39 +62,38 @@ def reprocess_data(filename, reprocess_nc, wind_data):
     else:
         raise ValueError(f'{wind_data} is not supported. Please use "ERA5" or "GEOS-FP"')
 
-    if reprocess_nc:
-        # read L2B data
-        l2b_filename = ('_'.join(plume_filename.split('_')[:-1])+'.nc').replace('L3', 'L2')
-        ds_l2b = xr.open_dataset(l2b_filename, decode_coords='all')
+    # read L2B data
+    l2b_filename = ('_'.join(plume_filename.split('_')[:-1])+'.nc').replace('L3', 'L2')
+    ds_l2b = xr.open_dataset(l2b_filename, decode_coords='all')
 
-        # read plume source location
-        longitude = df['plume_longitude'].item()
-        latitude = df['plume_latitude'].item()
+    # read plume source location
+    longitude = df['plume_longitude'].item()
+    latitude = df['plume_latitude'].item()
 
-        plume_num = re.search('plume(.*).nc', os.path.basename(plume_filename)).group(1)
-        plume_name = 'plume' + plume_num
+    plume_num = re.search('plume(.*).nc', os.path.basename(plume_filename)).group(1)
+    plume_name = 'plume' + plume_num
 
-        # create Emiss class
-        emiss = Emiss(ds=ds_l2b, gas=gas, plume_name=plume_name)
+    # create Emiss class
+    emiss = Emiss(ds=ds_l2b, gas=gas, plume_name=plume_name)
 
-        # select connected mask data
-        emiss.mask_data(longitude, latitude,
-                        wind_source=wind_source,
-                        land_only=land_only,
-                        land_mask_source=land_mask_source,
-                        only_plume=True,
-                        azimuth_diff_max=azimuth_diff_max,
-                        )
+    # select connected mask data
+    emiss.mask_data(longitude, latitude,
+                    wind_source=wind_source,
+                    land_only=land_only,
+                    land_mask_source=land_mask_source,
+                    only_plume=True,
+                    azimuth_diff_max=azimuth_diff_max,
+                    )
 
-        # export to NetCDF file
-        emiss.export_plume_nc()
+    # export to NetCDF file
+    emiss.export_plume_nc()
 
-        # calculate emission rate and export csv file
-        emiss.estimate(ipcc_sector, wspd_manual=None, land_only=land_only)
+    # calculate emission rate and export csv file
+    emiss.estimate(ipcc_sector, wspd_manual=None, land_only=land_only, name=name)
 
-        ds_l2b.close()
-        del emiss, ds_l2b
-        gc.collect()
+    ds_l2b.close()
+    del emiss, ds_l2b
+    gc.collect()
 
 
 def main():
@@ -104,16 +106,15 @@ def main():
 
     for filename in filelist:
         LOG.info(f'Reprocessing {filename} ...')
-        reprocess_data(filename, reprocess_nc, wind_data)
+        # regenerate the L3 plume NetCDF file based on csv settings and export new csv files
+        #   if users wanna change the alpha values, just change them in the `emiss.py` and run this script again
+        reprocess_data(filename, wind_data)
 
 
 if __name__ == '__main__':
     # root dir of hyper data
     root_dir = '/data/xinz/Hyper_TROPOMI_landfill/'
     lowest_dirs = get_dirs(root_dir)
-
-    # whether regenerate the L3 plume NetCDF file based on csv settings
-    reprocess_nc = True
 
     # which wind source data to be applied
     wind_data = 'auto'  # 'auto', 'ERA5', or 'GEOS-FP'
